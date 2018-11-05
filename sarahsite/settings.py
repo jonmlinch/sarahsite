8184e16460cc5f5e725bf1892134d815ebe7889b
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-11-05 15:13:10 -0800

    updated settings with postgresql and added app config

--- sarahsite/settings.py
+++ sarahsite/settings.py
@@ -76,8 +76,9 @@ WSGI_APPLICATION = 'sarahsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sarahsite',
+        'HOST': 'localhost'
     }
 }
 
